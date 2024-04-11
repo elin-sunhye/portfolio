3cd499d1f735e1d60ab89dd4fba6aa9b58a86549
sunhye <tjsgp43@gmail.com>
2024-04-11 17:10:32 +0900

    wiko 상세 페이지 작업중

--- app/(sub)/project/wiko/wiko.tsx
+++ app/(sub)/project/wiko/wiko.tsx
@@ -1,62 +1,87 @@
-"use client";
+'use client';
 
-import style from "./wiko.module.scss";
-import SubTop from "@/component/common/subTop/SubTop";
-import Rolling from "@/component/common/rolling/Rolling";
-import { MdFactory, MdFlight } from "react-icons/md";
-import { TbWorld } from "react-icons/tb";
+import style from './wiko.module.scss';
+import './swiper.scss';
+import SubTop from '@/component/common/subTop/SubTop';
+import Rolling from '@/component/common/rolling/Rolling';
+import { MdFactory, MdFlight } from 'react-icons/md';
+import { TbWorld } from 'react-icons/tb';
 
-// tree gragh
-import { Tree } from "react-tree-graph";
-import "react-tree-graph/dist/style.css";
+// react-swiper
+import { Swiper, SwiperSlide } from 'swiper/react'; // basic
+import 'swiper/css'; //basic
+import 'swiper/swiper-bundle.css';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { useRef } from 'react';
+import { HiOutlineChevronLeft, HiOutlineChevronRight } from 'react-icons/hi';
+import { Btn } from '@/component/common/btn/Btn';
 
 export default function Wiko() {
   // sub_top ---------------------------------
   const rollingItem1: { string: string; tag: JSX.Element }[] = [
-    { string: "123123123", tag: <></> },
-    { string: "", tag: <MdFlight role="img" aria-label="비행기 아이콘" /> },
-    { string: "axdjh1", tag: <></> },
-    { string: "alsald", tag: <></> },
+    { string: '123123123', tag: <></> },
+    { string: '', tag: <MdFlight role="img" aria-label="비행기 아이콘" /> },
+    { string: 'axdjh1', tag: <></> },
+    { string: 'alsald', tag: <></> },
     {
-      string: "",
+      string: '',
       tag: <TbWorld role="img" aria-label="세계 아이콘" />,
     },
-    { string: "bdfbsvxcv", tag: <></> },
-    { string: "csvsu7l", tag: <></> },
-    { string: "26md f", tag: <></> },
-    { string: "", tag: <MdFactory role="img" aria-label="공장 아이콘" /> },
-    { string: "sdfe wgw", tag: <></> },
+    { string: 'bdfbsvxcv', tag: <></> },
+    { string: 'csvsu7l', tag: <></> },
+    { string: '26md f', tag: <></> },
+    { string: '', tag: <MdFactory role="img" aria-label="공장 아이콘" /> },
+    { string: 'sdfe wgw', tag: <></> },
   ];
 
   const rollingItem2: { string: string; tag: JSX.Element }[] = [
-    { string: "", tag: <MdFlight role="img" aria-label="비행기 아이콘" /> },
-    { string: "axdjh1", tag: <></> },
-    { string: "123123123", tag: <></> },
-    { string: "alsald", tag: <></> },
+    { string: '', tag: <MdFlight role="img" aria-label="비행기 아이콘" /> },
+    { string: 'axdjh1', tag: <></> },
+    { string: '123123123', tag: <></> },
+    { string: 'alsald', tag: <></> },
     {
-      string: "",
+      string: '',
       tag: <TbWorld role="img" aria-label="세계 아이콘" />,
     },
-    { string: "bdfbsvxcv", tag: <></> },
-    { string: "", tag: <MdFactory role="img" aria-label="공장 아이콘" /> },
-    { string: "csvsu7l", tag: <></> },
-    { string: "26md f", tag: <></> },
-    { string: "sdfe wgw", tag: <></> },
+    { string: 'bdfbsvxcv', tag: <></> },
+    { string: '', tag: <MdFactory role="img" aria-label="공장 아이콘" /> },
+    { string: 'csvsu7l', tag: <></> },
+    { string: '26md f', tag: <></> },
+    { string: 'sdfe wgw', tag: <></> },
   ];
 
-  // point_section ---------------------------------
+  // data_section ---------------------------------
+  // swiper
+  const carreerRef = useRef(null);
+  // setting
+  const swiperParams = {
+    modules: [Navigation],
+    spaceBetween: 0, // 슬라이드 사이 여백
+    speed: 1000,
+    loop: true,
+    loopAdditionalSlides: 1,
+    slidesPerView: 1, //  한 슬라이드에 보여줄 개수
+    allowTouchMove: false, // false시에 스와이핑이 되지 않으며 버튼으로만 슬라이드 조작이 가능
+
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+  };
 
   return (
     <>
       {/* sub_top --------------------------------- */}
       <SubTop
         explain={
-          "rkskrkskrkrsm saja <br />jsgd asgd gasgd asg asdasdajsdh asjdha s"
+          'rkskrkskrkrsm saja <br />jsgd asgd gasgd asg asdasdajsdh asjdha s'
         }
         linkBtn={{
-          href: "http://wiko.co.kr",
-          title: "사이트",
-          id: "btnWiko",
+          href: 'http://wiko.co.kr',
+          title: 'view stie',
+          id: 'btnWiko',
           className: style.btn_link,
         }}
         bgColor="var(--main-blue-1)"
@@ -64,7 +89,7 @@ export default function Wiko() {
         <div className={`flex_start ${style.sub_top_inner}`}>
           <Rolling hoverStop={false} speed="120s">
             {rollingItem1.map((item, idx: number) => {
-              if (item.string === "") {
+              if (item.string === '') {
                 return (
                   <div
                     key={`ico_${idx}`}
@@ -88,7 +113,7 @@ export default function Wiko() {
 
           <Rolling hoverStop={false} deirection="right" speed="120s">
             {rollingItem2.map((item, idx: number) => {
-              if (item.string === "") {
+              if (item.string === '') {
                 return (
                   <div
                     key={`ico_${idx}`}
@@ -136,28 +161,136 @@ export default function Wiko() {
         </div>
       </section>
 
-      {/* point_section --------------------------------- */}
-      <section className={`section_padding ${style.point_section}`}>
+      {/* skill_sections --------------------------------- */}
+      <section className={`section_padding ${style.skill_section}`}>
         <div className={style.top_box}>
-          <p>code point</p>
+          <p>Technology Stack</p>
           <span>가나다 나다 가가라</span>
         </div>
 
-        <div className={style.point_box}>
-          <Tree
-            data={{
-              name: "Parent",
-              children: [
-                {
-                  name: "Child One",
-                },
-                {
-                  name: "Child Two",
-                },
-              ],
-            }}
-            height={400}
-            width={400}
+        <div className={`wrap ${style.skill_box}`}>
+          <div className={`flex_center ${style.skill_main}`}>사이트</div>
+          <div className={`${style.skill} ${style.skill_1}`}>REACT</div>
+          <div className={`${style.skill} ${style.skill_2}`}>REACT</div>
+          <div className={`${style.skill} ${style.skill_3}`}>REACT</div>
+          <div className={`${style.skill} ${style.skill_4}`}>REACT</div>
+          <div className={`${style.skill} ${style.skill_5}`}>REACT</div>
+          <div className={`${style.skill} ${style.skill_6}`}>REACT</div>
+        </div>
+      </section>
+
+      {/* code_section --------------------------------- */}
+      <section className={`section_padding ${style.code_section}`}>
+        <div className={style.top_box}>
+          <p>code</p>
+          <span>코드 어쩌고 저쩌고</span>
+        </div>
+
+        <div className={`wrap flex_start ${style.code_box}`}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+
+      {/* point_code_section --------------------------------- */}
+      <section className={`section_padding ${style.point_code_section}`}>
+        <div className={style.top_box}>
+          <p>point code</p>
+          <span>코드 어쩌고 저쩌고</span>
+        </div>
+
+        <div className={`wrap flex_start ${style.point_code_box}`}>
+          <div></div>
+          <div></div>
+        </div>
+      </section>
+
+      {/* data_section --------------------------------- */}
+      <section className={`section_padding ${style.data_section}`}>
+        <div className={style.top_box}>
+          <p>database</p>
+          <span>data 어쩌고 저쩌고</span>
+        </div>
+
+        <div className={`wrap ${style.data_box}`}>
+          <Rolling hoverStop={false} speed="90s">
+            <div className={style.data_item}>1123</div>
+            <div className={style.data_item}>sdf</div>
+            <div className={style.data_item}>135346</div>
+            <div className={style.data_item}>dfhg43</div>
+            <div className={style.data_item}>@$&^*</div>
+            <div className={style.data_item}>sdfv</div>
+            <div className={style.data_item}>125ygbe</div>
+            <div className={style.data_item}>1f4f</div>
+            <div className={style.data_item}>gn576</div>
+          </Rolling>
+        </div>
+      </section>
+
+      {/* data_code_section --------------------------------- */}
+      <section className={`section_padding ${style.data_code_section}`}>
+        <div className={style.top_box}>
+          <p>database</p>
+          <span>data 어쩌고 저쩌고</span>
+        </div>
+
+        <div className={`wrap ${style.data_code_box}`}>
+          <Swiper {...swiperParams} ref={carreerRef}>
+            <SwiperSlide>1</SwiperSlide>
+            <SwiperSlide>2</SwiperSlide>
+            <SwiperSlide>3</SwiperSlide>
+            <SwiperSlide>4</SwiperSlide>
+          </Swiper>
+          <Btn
+            type={`button`}
+            title={`이전 슬라이드 버튼`}
+            id={`swiperButtonPrev`}
+            className={`swiper-button-prev`}
+            btnType={'ico'}
+            ico={
+              <HiOutlineChevronLeft
+                role={`img`}
+                aria-label={`왼쪽 화살표 아이콘`}
+              />
+            }
+            hover={false}
+          />
+          <Btn
+            type={`button`}
+            title={`다음 슬라이드 버튼`}
+            id={`swiperButtonNext`}
+            className={`swiper-button-next`}
+            btnType={'ico'}
+            ico={
+              <HiOutlineChevronRight
+                role={`img`}
+                aria-label={`오른쪽 화살표 아이콘`}
+              />
+            }
+            hover={false}
+          />
+        </div>
+      </section>
+
+      {/* view_site_section --------------------------------- */}
+      <section
+        className={`section_padding flex_center ${style.view_site_section}`}
+      >
+        <div className={style.top_box}>
+          <h3>주조 어쩌고 저쩌고 짠</h3>
+          <span>보러고고</span>
+          <Btn
+            href={'http://wiko.co.kr'}
+            type={'link'}
+            title={'view site'}
+            id={'viewSite'}
+            btnType={'text'}
+            hover={false}
+            className={style.btn_go_page}
+            btnBg="var(--black)"
+            btnColor="var(--white)"
           />
         </div>
       </section>
